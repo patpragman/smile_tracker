dd6c982a27bd872102527939ae37bd1530a23af8
Patrick Pragman <patrickpragman@pop-os.localdomain>
2023-03-07 12:15:39 -0900

    NN around 50% accurate on test data

--- keras/cnn.py
+++ keras/cnn.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
+from notify import send_message_over_text
 
 from itertools import chain
 from keras import datasets, layers, models
 import matplotlib.pyplot as plt
 
 seed = 415
-batch_size = 64
+batch_size = 32
 
 train_images = tf.keras.preprocessing.image_dataset_from_directory(
     "images",
@@ -28,14 +29,14 @@ testing_images = tf.keras.preprocessing.image_dataset_from_directory(
 )
 
 my_callbacks = [
-    tf.keras.callbacks.EarlyStopping(monitor="val_accuracy", patience=5),
+    tf.keras.callbacks.EarlyStopping(monitor="val_accuracy", patience=2),
     tf.keras.callbacks.ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}.h5'),
-    tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+    #tf.keras.callbacks.TensorBoard(log_dir='./logs'),
 ]
 
 filter_shape = (4, 4)
 filter_count = 32
-dropout_value = 0.45
+dropout_value = 0.25
 pooling_value = 4
 
 
@@ -44,42 +45,28 @@ model = models.Sequential([
     # convolutional  starting size 48x48
     tf.keras.layers.Rescaling(1. / 255),  # size 45x45 inexplicably
 
-    layers.Conv2D(64, (4, 4), activation='relu', input_shape=(48, 48, 3), strides=1),
+    layers.Conv2D(64, (2, 2), activation='relu', input_shape=(48, 48, 3), strides=1),
     layers.Dropout(dropout_value),
     layers.MaxPooling2D(2, strides=1),
 
-    layers.Conv2D(512, (2, 2), activation='relu', strides=1),
+    layers.Conv2D(128, (2, 2), activation='relu', strides=1),
     layers.Dropout(dropout_value),
 
-    layers.Conv2D(512, (2, 2), activation='relu', strides=2),
+    layers.Conv2D(128, (2, 2), activation='relu', strides=2),
     layers.Dropout(dropout_value),
 
-    layers.Conv2D(512, (2, 2), activation='relu', strides=1),
-    layers.Dropout(dropout_value),
-
-    layers.Conv2D(512, (2, 2), activation='relu', strides=1),
-    layers.Dropout(dropout_value),
 
     layers.Conv2D(128, (2, 2), activation='relu', strides=1),
     layers.Dropout(dropout_value),
     layers.MaxPooling2D(2, strides=2),
 
-    layers.Conv2D(64, (2, 2), activation='relu', strides=1),
-    layers.Dropout(dropout_value),
-    layers.MaxPooling2D(2, strides=2),
 
 
 
     # flat layers
     layers.Flatten(),
-    layers.Dense(1024, activation="relu"),
-    layers.Dropout(dropout_value),
 
 
-    layers.Dense(1024, activation="relu"),
-    layers.Dropout(dropout_value),
-
-    layers.Dense(7, activation="relu"),
 
     layers.Dense(7, activation="softmax")
 
@@ -95,6 +82,10 @@ history = model.fit(train_images, epochs=100,
                     validation_data=testing_images,
                     callbacks=my_callbacks)
 
-print(model.summary())
 test_loss, test_acc = model.evaluate(testing_images, verbose=2)
 print(test_loss, test_acc)
+
+send_message_over_text(f'test loss:  {test_loss} \n{test_acc}')
+send_message_over_text(str(history.history))
+
+print(model.summary())
