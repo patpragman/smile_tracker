1074f2ed7691f9e70a1dcf50ba6e8b16b9844d3c
Patrick Pragman <patrickpragman@pop-os.localdomain>
2023-03-20 12:38:21 -0800

    73%

--- keras/cnn.py
+++ keras/cnn.py
@@ -48,7 +48,8 @@ resize_and_rescale = tf.keras.Sequential([
 my_callbacks = [
     tf.keras.callbacks.EarlyStopping(monitor="val_accuracy", patience=15),
     tf.keras.callbacks.ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}.h5'),
-    #tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+    tf.keras.callbacks.ReduceLROnPlateau(monitor="val_accuracy", factor=0.5,
+                                         patience=15, verbose=1)
 ]
 
 
@@ -59,51 +60,97 @@ dropout_value = 0.25
 model = models.Sequential([
     # convolutional  starting size 48x48
     resize_and_rescale,
-    data_augmentation,
+    data_augmentation,  # twisting and scaling
 
+    # first layer, look for features
     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 1), strides=2),
     layers.MaxPooling2D(pool_size=(2, 2)),
     layers.BatchNormalization(),
     layers.Dropout(dropout_value),
 
+    # second layer of feature extraction
     layers.Conv2D(64, kernel_size=(3, 3), activation='relu', padding="same"),
     layers.MaxPooling2D(pool_size=(2, 2)),
     layers.BatchNormalization(),
     layers.Dropout(dropout_value),
 
+    # third conv layer
     layers.Conv2D(128, kernel_size=(3, 3), activation='relu', padding="same"),
     layers.BatchNormalization(),
     layers.Dropout(dropout_value),
 
+    # 4th layer
     layers.Conv2D(256, kernel_size=(3, 3), activation='relu', padding="same"),
     layers.BatchNormalization(),
     layers.Dropout(dropout_value),
 
+    # 5th layer
     layers.Conv2D(512, kernel_size=(2, 2), activation='relu', padding="same"),
     layers.BatchNormalization(),
     layers.Dropout(dropout_value),
 
+    # 6th layer
     layers.Conv2D(1024, kernel_size=(3, 3), activation='relu', padding="same"),
     layers.BatchNormalization(),
     layers.Dropout(dropout_value),
 
+    # 7th layer
     layers.Conv2D(2048, kernel_size=(2, 2), activation='relu', padding="same"),
     layers.BatchNormalization(),
     layers.Dropout(dropout_value),
 
+    # 8th layer
     layers.Conv2D(1024, kernel_size=(4, 4), activation='relu', padding="same"),
     layers.BatchNormalization(),
     layers.Dropout(dropout_value),
 
+    # 9th layer
     layers.Conv2D(512, kernel_size=(2, 2), activation='relu', padding="same"),
     layers.BatchNormalization(),
     layers.Dropout(dropout_value),
 
+    # 10th layer
     layers.Conv2D(512, kernel_size=(2, 2), activation='relu', padding="same"),
     layers.BatchNormalization(),
     layers.Dropout(dropout_value),
 
-    layers.Conv2D(128, kernel_size=(4, 4), activation='relu'),
+    # 11th layer
+    layers.Conv2D(512, kernel_size=(2, 2), activation='relu', padding="same"),
+    layers.BatchNormalization(),
+    layers.Dropout(dropout_value),
+
+    # 12th layer
+    layers.Conv2D(512, kernel_size=(4, 4), activation='relu', padding="same"),
+    layers.BatchNormalization(),
+    layers.Dropout(dropout_value),
+
+    # 13th layer
+    layers.Conv2D(512, kernel_size=(3, 3), activation='relu', padding="same"),
+    layers.BatchNormalization(),
+    layers.Dropout(dropout_value),
+
+    # 14th layer
+    layers.Conv2D(512, kernel_size=(4, 4), activation='relu', padding="same"),
+    layers.BatchNormalization(),
+    layers.Dropout(dropout_value),
+
+    # 15th layer
+    layers.Conv2D(512, kernel_size=(2, 2), activation='relu', padding="same"),
+    layers.BatchNormalization(),
+    layers.Dropout(dropout_value),
+
+    # 16th layer
+    layers.Conv2D(512, kernel_size=(3, 3), activation='relu', padding="same"),
+    layers.BatchNormalization(),
+    layers.Dropout(dropout_value),
+
+    # 17th layer
+    layers.Conv2D(512, kernel_size=(2, 2), activation='relu', padding="same"),
+    layers.BatchNormalization(),
+    layers.Dropout(dropout_value),
+
+    # 18th layer
+    layers.Conv2D(128, kernel_size=(2, 2), activation='relu'),
     layers.MaxPooling2D(pool_size=(2, 2), padding="same"),
     layers.BatchNormalization(),
     layers.Dropout(dropout_value),
@@ -126,7 +173,7 @@ model.compile(optimizer='adam',
               metrics=['accuracy']
               )
 
-history = model.fit(train_images, epochs=100,
+history = model.fit(train_images, epochs=250,
                     validation_data=testing_images,
                     callbacks=my_callbacks)
 
