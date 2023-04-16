d7f581bb361309dcbcd953b6bb5d8f8adc3ddf4d
Patrick Pragman <patrickpragman@pop-os.localdomain>
2023-04-16 10:36:48 -0800

    updated work

--- example/notify.py
+++ example/notify.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 
 def send_message_over_text(msg):
     account_sid = 'AC5713b468de92a1d454a052432fb885bd'
-    auth_token = '392bc0da2b84a5328acfc65192c58e84'
+    auth_token = 'd97f12899673a9944d238efeed0424c4'
 
     client = Client(account_sid, auth_token)
 
@@ -14,3 +14,4 @@ def send_message_over_text(msg):
                          to='+19077825400'  # naturally, where you're sending it
                      )
 
+send_message_over_text("test")
